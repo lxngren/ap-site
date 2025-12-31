569abf444852d1de14a49c1bc2d923e53b6dc0e4
Vladislav <sailorsqq@gmail.com>
2025-12-31 03:08:44 +0300

    feat(routing): Update for domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ap-site/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
